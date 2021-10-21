e286b037acbdc1961cc947a022b28a2207cb3118
Lluis Morales Dobon <lmorales@cameparkare.com>
2021-10-21 21:41:31 +0200

    try to understand the mains of the tutorial

--- app/src/main/java/com/example/myfitapp/MainActivity.kt
+++ app/src/main/java/com/example/myfitapp/MainActivity.kt
@@ -2,10 +2,47 @@ package com.example.myfitapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var toggle : ActionBarDrawerToggle
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val drawerLayout : DrawerLayout = findViewById(R.id.drawerlayout)
+        val navView: NavigationView = findViewById(R.id.nav_view)
+
+        toggle = ActionBarDrawerToggle(this,drawerLayout,R.string.open,R.string.close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        navView.setNavigationItemSelectedListener{
+            when(it.itemId)
+            {
+                R.id.nav_MealInventory -> Toast.makeText(applicationContext, "Clicked MealInventory",Toast.LENGTH_SHORT).show()
+                R.id.nav_TrainingPlan -> Toast.makeText(applicationContext, "Clicked Training Plan",Toast.LENGTH_SHORT).show()
+                R.id.nav_Statistics -> Toast.makeText(applicationContext, "Clicked Statistics",Toast.LENGTH_SHORT).show()
+            }
+
+            true
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(toggle.onOptionsItemSelected(item))
+        {
+            return true
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
