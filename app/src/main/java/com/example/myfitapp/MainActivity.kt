6a36f9dab7233936055ed806dde092d6f30613d2
Lluis Morales Dobon <lmorales@cameparkare.com>
2021-10-24 00:59:10 +0200

    left menu engaging different fragments, and creating options menu right screen side

--- app/src/main/java/com/example/myfitapp/MainActivity.kt
+++ app/src/main/java/com/example/myfitapp/MainActivity.kt
@@ -2,39 +2,59 @@ package com.example.myfitapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
 import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
 import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var toggle : ActionBarDrawerToggle
+    lateinit var drawerLayout: DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val drawerLayout : DrawerLayout = findViewById(R.id.drawerlayout)
+        drawerLayout = findViewById(R.id.drawerlayout)
         val navView: NavigationView = findViewById(R.id.nav_view)
 
         toggle = ActionBarDrawerToggle(this,drawerLayout,R.string.open,R.string.close)
         drawerLayout.addDrawerListener(toggle)
         toggle.syncState()
 
+        //shows the upper left lateral menu
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         navView.setNavigationItemSelectedListener{
+
+            it.isChecked = true
+
             when(it.itemId)
             {
-                R.id.nav_MealInventory -> Toast.makeText(applicationContext, "Clicked MealInventory",Toast.LENGTH_SHORT).show()
-                R.id.nav_TrainingPlan -> Toast.makeText(applicationContext, "Clicked Training Plan",Toast.LENGTH_SHORT).show()
-                R.id.nav_Statistics -> Toast.makeText(applicationContext, "Clicked Statistics",Toast.LENGTH_SHORT).show()
+                R.id.nav_MealManagement -> ReplaceFragment(MealManagement(), it.title.toString())
+                R.id.nav_Statistics -> ReplaceFragment(StatisticsFragment(), it.title.toString())
+                R.id.nav_TrainingPlan -> ReplaceFragment(TrainingPlan(), it.title.toString())
             }
 
             true
         }
+    }
+
+    private fun ReplaceFragment(fragment: Fragment,title:String)
+    {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+
+        fragmentTransaction.replace(R.id.frameLayout, fragment)
+        fragmentTransaction.commit()
+        drawerLayout.closeDrawers()
+        setTitle(title)
+
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
